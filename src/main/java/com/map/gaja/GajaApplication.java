75f93bacf9d346c1fb947552ccb4002305734c02
Sangin <ksiisk99@gmail.com>
2023-04-22 14:26:55 +0900

    feat: 카카오 소셜 로그인

--- src/main/java/com/map/gaja/GajaApplication.java
+++ src/main/java/com/map/gaja/GajaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GajaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("good");
 		SpringApplication.run(GajaApplication.class, args);
 	}
 
