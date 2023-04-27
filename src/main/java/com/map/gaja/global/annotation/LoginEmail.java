22649bc14f7be58a3363818fad2d8bea1a9f428a
Jang_Desktop <sdsd090811@gmail.com>
2023-04-27 19:43:31 +0900

    feat: LoginEmail 어노테이션 생성 및 처리 리졸버 등록

---
+++ src/main/java/com/map/gaja/global/annotation/LoginEmail.java
@@ -0,0 +1,13 @@
+package com.map.gaja.global.annotation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginEmail {
+}
---
+++ src/main/java/com/map/gaja/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.map.gaja.global.config;
+
+import com.map.gaja.global.resolver.LoginEmailResolver;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginEmailResolver());
+    }
+}
