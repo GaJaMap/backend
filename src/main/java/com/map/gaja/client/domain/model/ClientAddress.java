309ba83d45abf8bb9b24dde618bae1e38a4cf276
Jang_Desktop <sdsd090811@gmail.com>
2023-05-04 17:03:45 +0900

    feat: 임베디드 타입에 Getter 추가

--- src/main/java/com/map/gaja/client/domain/model/ClientAddress.java
+++ src/main/java/com/map/gaja/client/domain/model/ClientAddress.java
@@ -1,10 +1,12 @@
 package com.map.gaja.client.domain.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
 @AllArgsConstructor
 public class ClientAddress {
     private String province; // 도/시/특별시
--- src/main/java/com/map/gaja/client/domain/model/ClientLocation.java
+++ src/main/java/com/map/gaja/client/domain/model/ClientLocation.java
@@ -1,10 +1,12 @@
 package com.map.gaja.client.domain.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
 @AllArgsConstructor
 public class ClientLocation {
     private Double latitude;
