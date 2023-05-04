4208e4b2977c16db58d7b99dff0b64a3ec491799
Jang_Desktop <sdsd090811@gmail.com>
2023-05-04 18:33:27 +0900

    test: findUser 테스트 코드 작성

---
+++ src/test/java/com/map/gaja/client/apllication/ClientServiceTest.java
@@ -0,0 +1,56 @@
+package com.map.gaja.client.apllication;
+
+import com.map.gaja.client.domain.model.Client;
+import com.map.gaja.client.infrastructure.repository.ClientRepository;
+import com.map.gaja.client.presentation.dto.response.ClientResponse;
+import com.map.gaja.client.presentation.exception.ClientNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ClientServiceTest {
+    @Mock
+    private ClientRepository repository;
+
+    @InjectMocks
+    private ClientService clientService;
+
+    @Test
+    public void testFindUser() throws Exception {
+        //given
+        Long searchId = 1L;
+        String searchName = "test";
+        Client findClient = mock(Client.class);
+        when(repository.findById(searchId)).thenReturn(Optional.ofNullable(findClient));
+        when(findClient.getId()).thenReturn(searchId);
+        when(findClient.getName()).thenReturn(searchName);
+
+        //when
+        ClientResponse response = clientService.findUser(searchId);
+
+        //then
+        assertThat(response.getClientId()).isEqualTo(searchId);
+        assertThat(response.getName()).isEqualTo(searchName);
+    }
+
+    @Test
+    public void testFindUserNotFound() throws Exception {
+        //given
+        Long searchId = 1L;
+        String searchName = "test";
+        Client findClient = mock(Client.class);
+        when(repository.findById(searchId)).thenReturn(Optional.empty());
+
+        //when
+        assertThatThrownBy(()->clientService.findUser(searchId))
+                .isInstanceOf(ClientNotFoundException.class);
+    }
+}
