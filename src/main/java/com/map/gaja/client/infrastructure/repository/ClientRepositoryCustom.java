b59f249db4fb643a2515f9a4512ace032462cf36
Jang_Laptop <sdsd090811@gmail.com>
2023-05-26 09:53:21 +0900

    feat: 키워드 검색 페이징 쿼리 작성

--- src/main/java/com/map/gaja/client/infrastructure/repository/ClientRepositoryCustom.java
+++ src/main/java/com/map/gaja/client/infrastructure/repository/ClientRepositoryCustom.java
@@ -2,10 +2,14 @@ package com.map.gaja.client.infrastructure.repository;
 
 import com.map.gaja.client.domain.model.Client;
 import com.map.gaja.client.presentation.dto.request.NearbyClientSearchRequest;
+import com.map.gaja.client.presentation.dto.response.ClientResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ClientRepositoryCustom {
     List<Client> mockFindClientByCondition(String name);
+    Page<ClientResponse> findClientByConditions(NearbyClientSearchRequest request, String keyword, Pageable pageable);
     List<Client> findClientsByLocation(NearbyClientSearchRequest request);
 }
--- src/main/java/com/map/gaja/client/infrastructure/repository/ClientRepositoryCustomImpl.java
+++ src/main/java/com/map/gaja/client/infrastructure/repository/ClientRepositoryCustomImpl.java
@@ -2,10 +2,22 @@ package com.map.gaja.client.infrastructure.repository;
 
 import com.map.gaja.client.domain.model.Client;
 import com.map.gaja.client.presentation.dto.request.NearbyClientSearchRequest;
+import com.map.gaja.client.presentation.dto.response.ClientResponse;
+import com.map.gaja.client.presentation.dto.subdto.AddressDto;
+import com.map.gaja.client.presentation.dto.subdto.LocationDto;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+
+import static com.map.gaja.client.domain.model.QClient.*;
 
 /**
  * QueryDSL없이 임시로 만듦.
@@ -13,6 +25,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ClientRepositoryCustomImpl implements ClientRepositoryCustom {
     private final EntityManager em;
+    private final JPAQueryFactory query;
 
     public List<Client> mockFindClientByCondition(String name) {
         List<Client> list = em.createQuery("Select c From Client c Where c.name like :name", Client.class)
@@ -31,5 +44,75 @@ public class ClientRepositoryCustomImpl implements ClientRepositoryCustom {
                 .setParameter("radius", request.getRadius())
                 .getResultList();
         return list;
+    }
+
+    public Page<ClientResponse> findClientByConditions(NearbyClientSearchRequest locationCond, String keyword, Pageable pageable) {
+        List<ClientResponse> result = query.select(
+                        Projections.constructor(ClientResponse.class,
+                                client.id,
+                                Expressions.asNumber(1L), // 임시 번들 데이터
+                                client.name,
+                                client.phoneNumber,
+                                Projections.constructor(
+                                        AddressDto.class,
+                                        client.address.province, client.address.city,
+                                        client.address.district, client.address.detail
+                                ),
+                                Projections.constructor(
+                                        LocationDto.class,
+                                        client.location.latitude,
+                                        client.location.longitude
+                                )
+                        )
+                )
+                .from(client)
+                .where(allContains(keyword), nearByUser(locationCond))
+                .orderBy(client.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = query.select(client.count())
+                .from(client)
+                .where(allContains(keyword), nearByUser(locationCond))
+                .fetchOne();
+
+        return new PageImpl<>(result, pageable, total);
+    }
+
+    private BooleanExpression nearByUser(NearbyClientSearchRequest locationCond) {
+        if(isLocationCondEmpty(locationCond)) {
+            return null;
+        }
+
+        // 반경 검색 조건은 일단 null
+        return null;
+    }
+
+    private boolean isLocationCondEmpty(NearbyClientSearchRequest locationCond) {
+        return locationCond == null
+                || locationCond.getLocation() == null
+                || locationCond.getRadius() == null;
+    }
+
+    private BooleanExpression allContains(String keywordCond) {
+        return nameContains(keywordCond)
+                .or(addressContains(keywordCond))
+                .or(phoneNumberContains(keywordCond));
+    }
+
+    private BooleanExpression phoneNumberContains(String phoneNumberCond) {
+        return phoneNumberCond != null ? client.phoneNumber.contains(phoneNumberCond) : null;
+    }
+
+    private BooleanExpression nameContains(String nameCond) {
+        return nameCond != null ? client.name.contains(nameCond) : null;
+    }
+
+    private BooleanExpression addressContains(String addressCond) {
+        return addressCond != null ? client.address.city.contains(addressCond)
+                .or(client.address.province.contains(addressCond))
+                .or(client.address.district.contains(addressCond))
+                .or(client.address.detail.contains(addressCond)) : null;
     }
 }
