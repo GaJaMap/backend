16b677ca9c3eb0fcf4b854c50744e668856068a5
Sangin <ksiisk99@gmail.com>
2023-05-30 00:12:05 +0900

    fix: 필드명 불일치로 프로젝션 적용이 제대로 안돼서 as로 필드명 일치시킴

--- src/main/java/com/map/gaja/bundle/infrastructure/BundleRepository.java
+++ src/main/java/com/map/gaja/bundle/infrastructure/BundleRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BundleRepository extends JpaRepository<Bundle, Long> {
-    @Query("SELECT b.id, b.name, b.clientCount FROM Bundle b WHERE b.user.id = :userId")
+    @Query("SELECT b.id as bundleId, b.name as bundleName, b.clientCount as clientCount FROM Bundle b WHERE b.user.id = :userId")
     Page<BundleInfo> findBundleByUserId(@Param(value = "userId") Long userId, Pageable pageable);
 }
