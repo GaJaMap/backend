dd882ea42a860057fe877da68c1a1a77b79d9100
Jang_Laptop <sdsd090811@gmail.com>
2023-05-31 17:33:52 +0900

    feat: CreatedClientResponse, CreatedClientListResponse 생성

---
+++ src/main/java/com/map/gaja/client/presentation/dto/response/CreatedClientResponse.java
@@ -0,0 +1,12 @@
+package com.map.gaja.client.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatedClientResponse {
+    private Long clientId;
+}
