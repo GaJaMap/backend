e23eb51c526630ac621a2bc92bb14c07901757e1
Jang_Desktop <sdsd090811@gmail.com>
2023-06-03 15:48:42 +0900

    feat: ST_Distance_Sphere를 사용하기 위해 Dialect에 등록

---
+++ src/main/java/com/map/gaja/global/config/MySQLCustomDialect.java
@@ -0,0 +1,12 @@
+package com.map.gaja.global.config;
+
+import org.hibernate.dialect.MySQL57Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.StandardBasicTypes;
+
+public class MySQLCustomDialect extends MySQL57Dialect {
+    public MySQLCustomDialect() {
+        super();
+        this.registerFunction("ST_Distance_Sphere",new StandardSQLFunction("ST_Distance_Sphere", StandardBasicTypes.DOUBLE));
+    }
+}
