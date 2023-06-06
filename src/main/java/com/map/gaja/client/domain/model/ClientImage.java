7f3d8ef07522c16b4bf2206f236034140255906f
Jang_Desktop <sdsd090811@gmail.com>
2023-06-06 15:48:34 +0900

    feat: ClientImage 엔티티 추가

---
+++ src/main/java/com/map/gaja/client/domain/model/ClientImage.java
@@ -0,0 +1,26 @@
+package com.map.gaja.client.domain.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClientImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "client_image_id")
+    private Long id;
+
+    private String originalName;
+
+    private String savedPath;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+}
