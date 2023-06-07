78d32b11c181ccdc86c61489079c7cdc3cc67288
Jang_Desktop <sdsd090811@gmail.com>
2023-06-07 19:58:00 +0900

    feat: 파일 저장 관련 로직 추가

---
+++ src/main/java/com/map/gaja/client/infrastructure/image/S3FileService.java
@@ -0,0 +1,99 @@
+package com.map.gaja.client.infrastructure.image;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.map.gaja.client.domain.exception.S3NotWorkingException;
+import com.map.gaja.client.presentation.dto.subdto.StoredFileDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+/**
+ * 저장경로: /{파일확장자}/{uuid}.{파일확장자}
+ * 파일 확장자 하위로 저장한다.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3FileService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public StoredFileDto storeFile(MultipartFile file) {
+        try (InputStream fileInputStream = file.getInputStream()) {
+            String s3StoredPath = storeFileToS3(file, fileInputStream);
+            log.info("저장 완료. S3 저장위치 = {}",s3StoredPath);
+            return new StoredFileDto(s3StoredPath, file.getOriginalFilename());
+        } catch (IOException e) {
+            log.warn("파일 저장 실패 file={}", file);
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean removeFile(String storedPath) {
+        if (!isStoredInS3(storedPath)) {
+            log.info("제거할 파일을 찾을 수 없습니다. storedPath={}", storedPath);
+            return false;
+        }
+
+        try {
+            amazonS3Client.deleteObject(bucket, storedPath);
+        } catch(RuntimeException e) {
+            log.error("S3 문제로 파일 제거 실패" , e);
+            throw new S3NotWorkingException(e);
+        }
+
+        return true;
+    }
+
+    private boolean isStoredInS3(String storedPath) {
+        return amazonS3Client.doesObjectExist(bucket, storedPath);
+    }
+
+    private String storeFileToS3(MultipartFile file, InputStream fileInputStream) {
+        String originalFilename = file.getOriginalFilename();
+        String storePath = getFilePath(originalFilename);
+
+        ObjectMetadata objectMetadata = getFileMetadata(file);
+
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket, storePath, fileInputStream, objectMetadata)
+        );
+
+        return amazonS3Client.getUrl(bucket, storePath).toString();
+    }
+
+    private String getFilePath(String originalFilename) {
+        String dirName = extractExt(originalFilename);
+        String storeFileName = createFileName(originalFilename);
+        return dirName + "/" + storeFileName;
+    }
+
+    private ObjectMetadata getFileMetadata(MultipartFile file) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        return objectMetadata;
+    }
+
+    private String createFileName(String originalFilename) {
+        String uuid = UUID.randomUUID().toString();
+        String ext = extractExt(originalFilename);
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+}
