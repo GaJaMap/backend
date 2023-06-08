3761f20ce3b5a885f3ceabe77ac94550beb17d62
Jang_Laptop <sdsd090811@gmail.com>
2023-06-08 18:02:02 +0900

    test: S3FileService 단위 테스트 수정

--- src/test/java/com/map/gaja/client/infrastructure/image/S3FileServiceTest.java
+++ src/test/java/com/map/gaja/client/infrastructure/image/S3FileServiceTest.java
@@ -1,16 +1,20 @@
 package com.map.gaja.client.infrastructure.image;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.map.gaja.client.domain.exception.InvalidFileException;
 import com.map.gaja.client.domain.exception.S3NotWorkingException;
 import com.map.gaja.client.presentation.dto.subdto.StoredFileDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -24,20 +28,20 @@ class S3FileServiceTest {
     @Mock
     AmazonS3Client awsS3Client;
 
+    @Mock
+    S3UrlGenerator s3UrlGenerator;
+
     @InjectMocks
     S3FileService s3FileService;
+
+    String s3Url = "http://bucket.s3.amazonaws.com/";
+    String originalFileName = "test.jpg";
+    String s3StoredPath = s3Url+originalFileName;
 
     @Test
     @DisplayName("파일 정상 저장")
     void storeFileTest() throws MalformedURLException {
-        String originalFileName = "test.jpg";
-        String s3StoredPath = "http://bucket.s3.amazonaws.com/jpg/test.jpg"; // 테스트를 위해 http로 작성
-        MockMultipartFile mockFile = new MockMultipartFile(
-                "file",
-                originalFileName,
-                "image/jpeg",
-                "test image files".getBytes()
-        );
+        MockMultipartFile mockFile = createMockFile();
 
         when(awsS3Client.putObject(any())).thenReturn(null);
         when(awsS3Client.getUrl(any(), any())).thenReturn(new URL(s3StoredPath));
@@ -49,12 +53,22 @@ class S3FileServiceTest {
     }
 
     @Test
+    @DisplayName("잘못된 파일이 들어옴")
+    void storeInvalidFileTest() throws IOException {
+        MockMultipartFile mockFile = Mockito.mock(MockMultipartFile.class);
+
+        when(mockFile.getInputStream()).thenThrow(IOException.class);
+
+        assertThrows(InvalidFileException.class, () -> s3FileService.storeFile(mockFile));
+    }
+
+    @Test
     @DisplayName("파일 정상 삭제")
     void removeFileSuccessTest() {
-        String storedPath = "jpg/test.jpg";
+        when(s3UrlGenerator.getS3Url()).thenReturn(s3Url);
         when(awsS3Client.doesObjectExist(any(), any())).thenReturn(true);
 
-        boolean result = s3FileService.removeFile(storedPath);
+        boolean result = s3FileService.removeFile(s3StoredPath);
 
         assertTrue(result);
     }
@@ -62,22 +76,30 @@ class S3FileServiceTest {
     @Test
     @DisplayName("제거할 파일 못찾음")
     void removeFileNotFoundTest() {
-        String storedPath = "jpg/test.jpg";
+        when(s3UrlGenerator.getS3Url()).thenReturn(s3Url);
         when(awsS3Client.doesObjectExist(any(), any())).thenReturn(false);
 
-        boolean result = s3FileService.removeFile(storedPath);
+        boolean result = s3FileService.removeFile(s3StoredPath);
 
         assertFalse(result);
     }
 
     @Test
-    @DisplayName("파일 저장 중 S3 에러 발생")
+    @DisplayName("파일 제거 중 S3 에러 발생")
     void removeS3FailTest() {
-        String storedPath = "jpg/test.jpg";
+        when(s3UrlGenerator.getS3Url()).thenReturn(s3Url);
         when(awsS3Client.doesObjectExist(any(), any())).thenReturn(true);
         doThrow(RuntimeException.class).when(awsS3Client).deleteObject(any(), any());
 
-        assertThrows(S3NotWorkingException.class, () -> s3FileService.removeFile(storedPath));
+        assertThrows(S3NotWorkingException.class, () -> s3FileService.removeFile(s3StoredPath));
     }
 
+    private MockMultipartFile createMockFile() {
+        return new MockMultipartFile(
+                "file",
+                originalFileName,
+                "image/jpeg",
+                "test image files".getBytes()
+        );
+    }
 }
