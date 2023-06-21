dad7e775d0cdd44ec79d56ca246efcc0c07876d4
Jang_Laptop <sdsd090811@gmail.com>
2023-06-21 18:27:24 +0900

    feat: Auditing 설정 추가

--- src/main/java/com/map/gaja/GajaApplication.java
+++ src/main/java/com/map/gaja/GajaApplication.java
@@ -2,7 +2,10 @@ package com.map.gaja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GajaApplication {
 
