bfbe2eb4f97c786b0fb69cb2d75ec2d0a62c1bde
Sangin <ksiisk99@gmail.com>
2023-06-21 11:56:44 +0900

    docs: 로그인 api명세서 작성

--- src/main/java/com/map/gaja/user/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/map/gaja/user/presentation/dto/request/LoginRequest.java
@@ -1,10 +1,12 @@
 package com.map.gaja.user.presentation.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class LoginRequest {
-    private final String accessToken;
+    @Schema(description = "카카오 액세스 토큰")
+    private String accessToken;
 }
