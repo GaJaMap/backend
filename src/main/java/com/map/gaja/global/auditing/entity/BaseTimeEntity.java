7fa487afaa90c143dc78e33332b102dcdab5ca68
Jang_Desktop <sdsd090811@gmail.com>
2023-06-22 17:44:02 +0900

    style: createAt -> createdAt으로 리네임

--- src/main/java/com/map/gaja/global/auditing/entity/BaseTimeEntity.java
+++ src/main/java/com/map/gaja/global/auditing/entity/BaseTimeEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class BaseTimeEntity {
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
