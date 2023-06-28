adb01a8ff1d500b95e7bc1dc4b60c35f354407c1
Jang_Desktop <sdsd090811@gmail.com>
2023-06-28 16:38:17 +0900

    fix: BundleAccessVerifyService 의존성 추가

--- src/main/java/com/map/gaja/client/apllication/ClientAccessVerifyService.java
+++ src/main/java/com/map/gaja/client/apllication/ClientAccessVerifyService.java
@@ -1,5 +1,6 @@
 package com.map.gaja.client.apllication;
 
+import com.map.gaja.bundle.application.BundleAccessVerifyService;
 import com.map.gaja.bundle.domain.exception.BundleNotFoundException;
 import com.map.gaja.bundle.infrastructure.BundleQueryRepository;
 import com.map.gaja.client.infrastructure.repository.ClientQueryRepository;
@@ -12,17 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ClientAccessVerifyService {
     private final ClientQueryRepository clientQueryRepository;
-    private final BundleQueryRepository bundleQueryRepository;
+    private final BundleAccessVerifyService bundleAccessVerifyService;
 
     public void verifyClientAccess(ClientAccessCheckDto accessRequest) {
         String userEmail = accessRequest.getUserEmail();
         long bundleId = accessRequest.getBundleId();
         long clientId = accessRequest.getClientId();
 
-        if(bundleQueryRepository.hasNoBundleByUser(bundleId, userEmail)) {
-            throw new BundleNotFoundException();
-        }
-
+        bundleAccessVerifyService.verifyBundleAccess(bundleId, userEmail);
         if (clientQueryRepository.hasNoClientByBundle(bundleId, clientId)) {
             throw new ClientNotFoundException(clientId);
         }
