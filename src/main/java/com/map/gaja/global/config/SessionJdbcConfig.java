d592f8d851d8fd192bd6cc350adeb11906543e64
Sangin <ksiisk99@gmail.com>
2023-07-01 17:18:04 +0900

    fix: 스프링 부트 자동 환경설정이 있어서 DataSourceTransactionManager Bean 등록코드 제거

--- src/main/java/com/map/gaja/global/config/SessionJdbcConfig.java
+++ src/main/java/com/map/gaja/global/config/SessionJdbcConfig.java
@@ -1,17 +1,8 @@
 package com.map.gaja.global.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
 
 @EnableJdbcHttpSession(tableName = "session", maxInactiveIntervalInSeconds = 3600)
 public class SessionJdbcConfig {
 
-    @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
 }
