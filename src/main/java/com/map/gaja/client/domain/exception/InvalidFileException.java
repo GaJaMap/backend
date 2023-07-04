8deb65850a83cd3e89b45558e08392d7a10cee7f
Jang_Laptop <sdsd090811@gmail.com>
2023-07-04 15:36:52 +0900

    fix: InvalidFileException이 BusinessException을 상속하도록 변경

--- src/main/java/com/map/gaja/client/domain/exception/InvalidFileException.java
+++ src/main/java/com/map/gaja/client/domain/exception/InvalidFileException.java
@@ -1,15 +1,14 @@
 package com.map.gaja.client.domain.exception;
 
+import com.map.gaja.global.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
-public class InvalidFileException extends RuntimeException {
-    private final String message = "잘못된 파일 형식입니다.";
-    private final HttpStatus status = HttpStatus.BAD_REQUEST;
-
+public class InvalidFileException extends BusinessException {
     public InvalidFileException() {
+        super(HttpStatus.BAD_REQUEST, "잘못된 파일입니다.");
     }
 
     public InvalidFileException(Throwable cause) {
-        super(cause);
+        super(cause, HttpStatus.BAD_REQUEST, "잘못된 파일입니다.");
     }
 }
