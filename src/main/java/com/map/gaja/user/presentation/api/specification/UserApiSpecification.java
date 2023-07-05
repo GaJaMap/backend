65aeb5f3a6ef7ffc4910922c77181028cfce8b7c
Sangin <ksiisk99@gmail.com>
2023-07-05 15:46:07 +0900

    remove: 불필요한 파일과 코드 제거

--- src/main/java/com/map/gaja/user/presentation/api/specification/UserApiSpecification.java
+++ src/main/java/com/map/gaja/user/presentation/api/specification/UserApiSpecification.java
@@ -2,8 +2,6 @@ package com.map.gaja.user.presentation.api.specification;
 
 import com.map.gaja.global.exception.ExceptionDto;
 import com.map.gaja.user.presentation.dto.request.LoginRequest;
-import com.map.gaja.user.presentation.dto.request.Req;
-import com.map.gaja.user.presentation.dto.response.Res;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,15 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface UserApiSpecification {
-    @Operation(summary = "api 설명에 대한 간단 요약",
-            description = "세부사항",
-            //parameters = {@Parameter(name = "token", description = "액세스 토큰")},
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Res.class))),
-                    @ApiResponse(responseCode = "404", description = "존재하지 않는 회원입니다.", content = @Content(schema = @Schema(implementation = ExceptionDto.class)))
-            }
-    )
-    Res test(Req req);
 
     @Operation(summary = "로그인 요청",
             description = "카카오 액세스토큰을 이용한 로그인",
