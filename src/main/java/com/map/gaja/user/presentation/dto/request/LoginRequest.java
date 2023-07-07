7b7509606fd5d2d7e6a6e053b235b4b6a38b4e45
Sangin <ksiisk99@gmail.com>
2023-07-07 11:37:13 +0900

    fix: Type-Mismatch로 인한 기본생성자 추가

--- src/main/java/com/map/gaja/user/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/map/gaja/user/presentation/dto/request/LoginRequest.java
@@ -3,8 +3,10 @@ package com.map.gaja.user.presentation.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
     @Schema(description = "카카오 액세스 토큰")
