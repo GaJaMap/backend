eae6a146cb3a1e940d0e9f74bd2419a65aca44bf
Jang_Desktop <sdsd090811@gmail.com>
2023-07-12 20:21:21 +0900

    docs: 이미지가 필요한 곳에 422 에러 코드 추가

--- src/main/java/com/map/gaja/client/presentation/api/specification/ClientCommandApiSpecification.java
+++ src/main/java/com/map/gaja/client/presentation/api/specification/ClientCommandApiSpecification.java
@@ -42,6 +42,7 @@ public interface ClientCommandApiSpecification {
                     @ApiResponse(responseCode = "200", description = "성공"),
                     @ApiResponse(responseCode = "400", description = "위치 정보가 한국을 벗어남", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
                     @ApiResponse(responseCode = "404", description = "사용자에게 요청 그룹이 없거나, 그룹에 요청 고객이 없음", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
+                    @ApiResponse(responseCode = "422", description = "서버에서 지원하지 않는 파일 형식", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
                     @ApiResponse(responseCode = "500", description = "서버 내부 오류로 이미지 저장 실패", content = @Content(schema = @Schema(implementation = ExceptionDto.class)))
             })
     ResponseEntity<Void> changeClient(
@@ -60,6 +61,7 @@ public interface ClientCommandApiSpecification {
                     @ApiResponse(responseCode = "201", description = "성공 - 고객 ID 반환", content = @Content(schema = @Schema(implementation = Long.class))),
                     @ApiResponse(responseCode = "400", description = "사용자 정보가 한국을 벗어남", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
                     @ApiResponse(responseCode = "404", description = "사용자에게 요청 그룹이 없거나, 그룹에 요청 고객이 없음", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
+                    @ApiResponse(responseCode = "422", description = "서버에서 지원하지 않는 파일 형식", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
                     @ApiResponse(responseCode = "500", description = "서버 내부 오류로 이미지 저장 실패", content = @Content(schema = @Schema(implementation = ExceptionDto.class)))
             })
     ResponseEntity<Long> addClient(
