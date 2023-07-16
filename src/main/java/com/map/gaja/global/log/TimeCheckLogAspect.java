a5daa3a889ddaa3ac946fa142546748cd379e7e1
Jang_Desktop <sdsd090811@gmail.com>
2023-07-17 00:26:37 +0900

    feat: Method에 어노테이션 적용할 수 있도록 수정

--- src/main/java/com/map/gaja/global/log/TimeCheckLogAspect.java
+++ src/main/java/com/map/gaja/global/log/TimeCheckLogAspect.java
@@ -14,20 +14,23 @@ import org.springframework.stereotype.Component;
 public class TimeCheckLogAspect {
     private static final double NANOSECONDS_TO_MILLISECONDS = 1_000_000.0;
 
-    @Around("@annotation(com.map.gaja.global.log.TimeCheckLog)")
-    public Object checkTime(ProceedingJoinPoint pjp) {
+    @Around("@within(com.map.gaja.global.log.TimeCheckLog) || @annotation(com.map.gaja.global.log.TimeCheckLog)")
+    public Object checkTime(ProceedingJoinPoint pjp) throws Exception {
         Object result = null;
         long start, end;
 
         start = System.nanoTime();
         Signature sig = pjp.getSignature();
         Object[] args = pjp.getArgs();
+        String classDotMethod = getClassDotMethod(pjp);
 
-        log.info("{} 메소드 시작. 파라미터: {}", sig.getName(), args);
+        log.info("{} 메소드 시작. 파라미터: {}", classDotMethod, args);
         try {
             result = pjp.proceed(); // 핵심 기능 실행
             return result;
         } catch (BusinessException e) {
+            throw e;
+        } catch (Exception e) {
             throw e;
         } catch (Throwable e) {
             throw new RuntimeException(e);
@@ -35,8 +38,18 @@ public class TimeCheckLogAspect {
             end = System.nanoTime();
             long runningTimeNano = end - start;
             double runningTimeMillis = runningTimeNano / NANOSECONDS_TO_MILLISECONDS;
-            log.info("{} 메소드 동작 시간: {}ms", sig.getName(), runningTimeMillis);
+            log.info("{} 메소드 동작 시간: {}ms", classDotMethod, runningTimeMillis);
         }
+    }
+
+    /**
+     * {클래스명}.{메소드명}으로 반환
+     */
+    private static String getClassDotMethod(ProceedingJoinPoint pjp) {
+        String targetClassPath = pjp.getTarget().getClass().toString();
+        int classNameIdx = targetClassPath.lastIndexOf(".") + 1;
+        Signature signature = pjp.getSignature();
+        return targetClassPath.substring(classNameIdx) + "." + signature.getName();
     }
 
 }
