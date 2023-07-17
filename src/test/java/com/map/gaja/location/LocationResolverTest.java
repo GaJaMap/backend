7ee0e12075f350af2d50d731687d4a8c09bffba9
Sangin <ksiisk99@gmail.com>
2023-07-17 20:32:19 +0900

    test: 도로명 주소를 위도 경도로 변환 테스트

---
+++ src/test/java/com/map/gaja/location/LocationResolverTest.java
@@ -0,0 +1,29 @@
+package com.map.gaja.location;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.map.gaja.client.infrastructure.file.excel.ClientExcelData;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class LocationResolverTest {
+
+    @Test
+    void convertCoordinate() {
+        LocationResolver resolver = new LocationResolver(new ObjectMapper());
+        long startTime = System.nanoTime();
+
+        List<ClientExcelData> addresses = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            addresses.add(new ClientExcelData("", "", "서울특별시 강남구", "", null, null));
+        }
+        resolver.convertCoordinate(addresses);
+
+        long executionTime = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Time: " + executionTime + " ms");
+
+        addresses.stream()
+                .forEach(i -> System.out.println(i.getLocation().getLatitude()));
+    }
+}
