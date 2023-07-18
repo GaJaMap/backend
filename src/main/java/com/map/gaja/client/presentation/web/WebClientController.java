0a2d0256f91d9e6ed5374250a27924474fc6166d
Sangin <ksiisk99@gmail.com>
2023-07-19 01:35:08 +0900

    fix: 엑셀 업로드 페이지 @AuthenticationPrincipal 매개변수 타입 수정과 로그인이 안된 사용자 카카오 로그인 페이지로 리다이렉트

--- src/main/java/com/map/gaja/client/presentation/web/WebClientController.java
+++ src/main/java/com/map/gaja/client/presentation/web/WebClientController.java
@@ -6,12 +6,14 @@ import com.map.gaja.client.infrastructure.file.excel.ClientExcelData;
 import com.map.gaja.client.infrastructure.file.excel.ExcelParser;
 import com.map.gaja.client.presentation.dto.request.ClientExcelRequest;
 import com.map.gaja.client.presentation.dto.subdto.GroupInfoDto;
+import com.map.gaja.global.authentication.PrincipalDetails;
 import com.map.gaja.group.application.GroupAccessVerifyService;
 import com.map.gaja.group.application.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,15 +33,17 @@ public class WebClientController {
 
     @GetMapping("/")
     public String clientFileUpload(
-            @AuthenticationPrincipal String loginEmail,
+            @AuthenticationPrincipal PrincipalDetails authentication,
             Model model
     ) {
         // 로그인이 안된 사용자
-        if (loginEmail.equals("anonymousUser")) {
-            return "redirect:/login";
+        if (authentication == null) {
+            return "redirect:/oauth2/authorization/kakao";
         }
 
-        List<GroupInfoDto> activeGroupInfo = groupService.findActiveGroupInfo(loginEmail);
+        String email = authentication.getName(); //이메일
+
+        List<GroupInfoDto> activeGroupInfo = groupService.findActiveGroupInfo(email);
         model.addAttribute("groupList", activeGroupInfo);
 
         return "index";
