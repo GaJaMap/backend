3604f5426c63dca02d16f3105dec789f423ef906
Jang_Desktop <sdsd090811@gmail.com>
2023-07-19 17:57:32 +0900

    feat: 그룹 내의 특정 고객들을 제거하기 위한 벌크 쿼리 추가

--- src/main/java/com/map/gaja/client/infrastructure/repository/ClientRepository.java
+++ src/main/java/com/map/gaja/client/infrastructure/repository/ClientRepository.java
@@ -13,4 +13,33 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     @Modifying
     @Query("DELETE FROM Client c WHERE c.group.id = :groupId")
     void deleteByGroupId(@Param(value = "groupId") Long groupId);
+
+
+    /**
+     * 그룹 내에 특정 고객들 제거를 위한 기능
+     * clientIds와 관련된 ClientImage의 isDeleted 필드를 true로 표시
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("UPDATE ClientImage ci SET ci.isDeleted = true " +
+            "WHERE ci.id IN (SELECT c.clientImage.id FROM Client c WHERE c.id IN :clientIds)")
+    void markClientImageAsDeleted(List<Long> clientIds);
+
+
+    /**
+     * 그룹 내에 특정 고객들 제거를 위한 기능
+     * Client와 ClientImage의 연관 관계를 끊음
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("UPDATE Client c SET c.clientImage = null " +
+            "WHERE c.id IN :clientIds")
+    void setClientForeignKeyToNull(List<Long> clientIds);
+
+    /**
+     * 그룹 내에 특정 고객들 제거를 위한 기능
+     * clientIds에 있는 Client 전부 제거
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("DELETE FROM Client c " +
+            "WHERE c.id IN :clientIds")
+    void deleteClientsInClientIds(List<Long> clientIds);
 }
