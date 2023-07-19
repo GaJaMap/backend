589376c16afd54d7a39d11bafcddee861bbdca04
Jang_Desktop <sdsd090811@gmail.com>
2023-07-19 15:21:59 +0900

    feat: client id 리스트 접근 검증 로직 추가

--- src/main/java/com/map/gaja/client/apllication/ClientAccessVerifyService.java
+++ src/main/java/com/map/gaja/client/apllication/ClientAccessVerifyService.java
@@ -1,5 +1,6 @@
 package com.map.gaja.client.apllication;
 
+import com.map.gaja.client.presentation.dto.ClientListAccessCheckDto;
 import com.map.gaja.group.application.GroupAccessVerifyService;
 import com.map.gaja.client.infrastructure.repository.ClientQueryRepository;
 import com.map.gaja.client.presentation.dto.ClientAccessCheckDto;
@@ -7,6 +8,8 @@ import com.map.gaja.client.domain.exception.ClientNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -25,4 +28,20 @@ public class ClientAccessVerifyService {
             throw new ClientNotFoundException();
         }
     }
+    public void verifyClientListAccess(ClientListAccessCheckDto accessRequest) {
+        String userEmail = accessRequest.getUserEmail();
+        long groupId = accessRequest.getGroupId();
+        List<Long> clientIds = accessRequest.getClientId();
+
+        groupAccessVerifyService.verifyGroupAccess(groupId, userEmail);
+        long matchingCount = clientQueryRepository.findMatchingClientCountInGroup(groupId, clientIds);
+        if (areSomeClientsNotFound(clientIds, matchingCount)) {
+            throw new ClientNotFoundException();
+        }
+    }
+
+    private static boolean areSomeClientsNotFound(List<Long> clientIds, long matchingCount) {
+        return clientIds.size() != matchingCount;
+    }
+
 }
---
+++ src/main/java/com/map/gaja/client/presentation/dto/ClientListAccessCheckDto.java
@@ -0,0 +1,16 @@
+package com.map.gaja.client.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientListAccessCheckDto {
+    private String userEmail;
+    private long groupId;
+    private List<Long> clientId;
+}
