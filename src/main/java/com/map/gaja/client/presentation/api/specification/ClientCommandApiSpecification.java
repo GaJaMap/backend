02d07ba01721180600cbba6a0d3cab11fd28fb61
Jang_Desktop <sdsd090811@gmail.com>
2023-07-20 18:56:10 +0900

    docs: DELETE /group/{groupId}/clients/bulk 스웨거 문서 추가

--- src/main/java/com/map/gaja/client/presentation/api/specification/ClientCommandApiSpecification.java
+++ src/main/java/com/map/gaja/client/presentation/api/specification/ClientCommandApiSpecification.java
@@ -1,5 +1,7 @@
 package com.map.gaja.client.presentation.api.specification;
 
+import com.map.gaja.client.presentation.dto.access.ClientListAccessCheckDto;
+import com.map.gaja.client.presentation.dto.request.ClientIdsRequest;
 import com.map.gaja.client.presentation.dto.request.NewClientRequest;
 
 import com.map.gaja.client.presentation.dto.request.simple.SimpleClientBulkRequest;
@@ -12,6 +14,7 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindException;
@@ -36,6 +39,22 @@ public interface ClientCommandApiSpecification {
             @Schema(hidden = true) @AuthenticationPrincipal String loginEmail,
             @PathVariable Long groupId,
             @PathVariable Long clientId
+    );
+
+    @Operation(summary = "다수 고객 삭제",
+            parameters = {
+                    @Parameter(name = "JSESSIONID", description = "세션 ID", in = ParameterIn.HEADER),
+            },
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ValidationErrorInfo.class)))),
+                    @ApiResponse(responseCode = "422", description = "사용자에게 요청 그룹이 없거나 그룹에 요청 고객이 없음 <br> 또는 그룹 내에 사용자 수보다 많은 사용자를 제거할 수 없음", content = @Content(schema = @Schema(implementation = ExceptionDto.class)))
+            })
+    @DeleteMapping("/group/{groupId}/clients/bulk")
+    public ResponseEntity<Void> deleteBulkClient(
+            @Schema(hidden = true) @AuthenticationPrincipal String loginEmail,
+            @PathVariable Long groupId,
+            @Valid @RequestBody ClientIdsRequest clientIdsRequest
     );
 
     @Operation(summary = "고객 정보 변경", description = "form 형식으로 clientRequest 데이터를 전달해주세요",
