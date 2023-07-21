e8c9e7ea9cb29152d83c172c7b2a2e68768f673c
Jang_Desktop <sdsd090811@gmail.com>
2023-07-21 14:19:38 +0900

    feat: 위도 경도에 NotNull 제약 추가

--- src/main/java/com/map/gaja/client/presentation/dto/request/subdto/LocationDto.java
+++ src/main/java/com/map/gaja/client/presentation/dto/request/subdto/LocationDto.java
@@ -6,13 +6,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationDto {
+    @NotNull
     @Range(min = 33, max = 38, message = "위도는 한국 위도 범위인 +33.0 이상, +38 이하만 가능합니다.")
     @Schema(description = "현재 사용자 위도", example = "33.12345")
     private Double latitude;
+
+    @NotNull
     @Range(min = 124, max = 132, message = "경도는 한국 경도 범위인 +124.0 이상, +132.0 이하만 가능합니다.")
     @Schema(description = "현재 사용자 경도", example = "127.7777")
     private Double longitude;
