371cc3703da53c6b4a579b03b9b7cef8ae3f5012
Sangin <ksiisk99@gmail.com>
2023-08-07 19:21:06 +0900

    feat: JsonProcessingException 예외 추적을 위한 생성자 추가

--- src/main/java/com/map/gaja/location/exception/NotExcelUploadException.java
+++ src/main/java/com/map/gaja/location/exception/NotExcelUploadException.java
@@ -7,4 +7,8 @@ public class NotExcelUploadException extends BusinessException {
     public NotExcelUploadException() {
         super(HttpStatus.BAD_REQUEST, "현재 엑셀 업로드 서비스를 사용할 수 없습니다.");
     }
+
+    public NotExcelUploadException(Throwable e) {
+        super(e, HttpStatus.BAD_REQUEST, "현재 엑셀 업로드 서비스를 사용할 수 없습니다.");
+    }
 }
