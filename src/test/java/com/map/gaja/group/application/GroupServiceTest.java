13bf6de9fe4769ae4309b2ea82b609e588fd5c1b
Sangin <ksiisk99@gmail.com>
2023-08-12 02:32:56 +0900

    test: 그룹 생성 성공 테스트

--- src/test/java/com/map/gaja/group/application/GroupServiceTest.java
+++ src/test/java/com/map/gaja/group/application/GroupServiceTest.java
@@ -1,11 +1,13 @@
 package com.map.gaja.group.application;
 
+import com.map.gaja.group.domain.model.Group;
 import com.map.gaja.group.infrastructure.GroupRepository;
 import com.map.gaja.group.presentation.dto.request.GroupCreateRequest;
 import com.map.gaja.user.domain.exception.GroupLimitExceededException;
 import com.map.gaja.user.domain.model.Authority;
 import com.map.gaja.user.domain.model.User;
 import com.map.gaja.user.infrastructure.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,7 +19,10 @@ import java.time.ZoneId;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.when;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class GroupServiceTest {
@@ -31,7 +36,8 @@ class GroupServiceTest {
     GroupService groupService;
 
     @Test
-    void 그룹_생성_실패() {
+    @DisplayName("등급 제한으로 그룹 생성 실패")
+    void createGroupFail() {
         String email="test@gmail.com";
         GroupCreateRequest groupCreateRequest = new GroupCreateRequest("bundle");
         User user = User.builder()
@@ -45,6 +51,22 @@ class GroupServiceTest {
         when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
 
         assertThatThrownBy(()-> groupService.create(email, groupCreateRequest)).isInstanceOf(GroupLimitExceededException.class);
+    }
+
+    @Test
+    @DisplayName("그룹 생성 성공")
+    void createGroupSuccess() {
+        String email="test@gmail.com";
+        GroupCreateRequest groupCreateRequest = new GroupCreateRequest("group");
+        User user = new User(email);
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(groupRepository.save(any())).thenReturn(new Group("group", user));
+
+        Long groupId = groupService.create(email, groupCreateRequest);
+
+        verify(groupRepository, times(1)).save(any());
+        assertEquals(1, user.getGroupCount());
     }
 
 }
