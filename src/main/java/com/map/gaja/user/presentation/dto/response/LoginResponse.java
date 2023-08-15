c431f8c1cf66ec8ef657a130d1185a0f97967dca
Sangin <ksiisk99@gmail.com>
2023-08-15 16:50:24 +0900

    feat: 로그인 성공 응답 값 설정페이지에 사용자 정보에 맞게 필드 값 수정

--- src/main/java/com/map/gaja/user/presentation/dto/response/LoginResponse.java
+++ src/main/java/com/map/gaja/user/presentation/dto/response/LoginResponse.java
@@ -7,9 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class LoginResponse {
-    @Schema(description = "최근에 참조한 그룹아이디 => null이면 전체 고객 검색 api호출 / null이 아니면 특정 그룹 고객 전부 조회 api호출")
-    private Long groupId;
+    @Schema(description = "이메일")
+    private String email;
 
     @Schema(description = "사용자 권한 등급 FREE(그룹1개, 각 그룹당 고객 200명), VIP(그룹500개, 각 그룹당 고객 1000명)")
     private String authority;
+
+    @Schema(description = "생성일")
+    private String createdDate;
+
+
+
 }
