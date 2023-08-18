37dbbf79aa1701d61b8c9f8f0af3cd477941e65d
Jang_Desktop <sdsd090811@gmail.com>
2023-08-18 19:56:40 +0900

    feat: Postgis의 ST_dwithin을 사용한 반경 검색 쿼리 추가

--- src/main/java/com/map/gaja/client/infrastructure/repository/querydsl/sql/NativeSqlCreator.java
+++ src/main/java/com/map/gaja/client/infrastructure/repository/querydsl/sql/NativeSqlCreator.java
@@ -1,9 +1,11 @@
 package com.map.gaja.client.infrastructure.repository.querydsl.sql;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.spatial.locationtech.jts.JTSPointPath;
 import org.locationtech.jts.geom.Point;
 
 public interface NativeSqlCreator {
     NumberExpression<Double> createCalcDistanceSQL(Point constant, JTSPointPath location);
+    BooleanExpression createRadiusSearchSQL(Point currentLocation, JTSPointPath dbLocation, int distance);
 }
