53feb2a2e68c4452d961985c7abf0a710f5e90b1
Sangin <ksiisk99@gmail.com>
2023-08-24 01:29:44 +0900

    docs: 자동로그인 GroupInfo에 대한 명세서 수정

--- src/main/java/com/map/gaja/user/presentation/dto/response/AutoLoginResponse.java
+++ src/main/java/com/map/gaja/user/presentation/dto/response/AutoLoginResponse.java
@@ -12,6 +12,6 @@ public class AutoLoginResponse {
     @Schema(description = "고객 api 호출한 클래스와 동일")
     private ClientListResponse clientListResponse;
 
-    @Schema(description = "최근에 참조한 그룹 정보인데 NULL이면 전체이고, NULL이 아니면 특정 그룹에 대한 정보가 있으니 앱에서 NULL을 분기처리 해줘야함")
+    @Schema(description = "최근에 참조한 그룹 정보인데 '전체'라면 groupId가 -1임")
     private GroupInfo groupInfo;
 }
