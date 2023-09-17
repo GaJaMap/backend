41a226a88c8b4f4e8189a66d96a9c6a316f553d8
Jang_Desktop <sdsd090811@gmail.com>
2023-09-17 12:11:42 +0900

    fix: 위도 경도를 null을 허용하도록 수정

--- src/main/java/com/map/gaja/client/presentation/dto/request/subdto/LocationDto.java
+++ src/main/java/com/map/gaja/client/presentation/dto/request/subdto/LocationDto.java
@@ -14,12 +14,12 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationDto {
-    @NotNull
+//    @NotNull
     @Range(min = 33, max = 38, message = "위도는 한국 위도 범위인 +33.0 이상, +38 이하만 가능합니다.")
     @Schema(description = "현재 사용자 위도", example = "33.12345")
     private Double latitude;
 
-    @NotNull
+//    @NotNull
     @Range(min = 124, max = 132, message = "경도는 한국 경도 범위인 +124.0 이상, +132.0 이하만 가능합니다.")
     @Schema(description = "현재 사용자 경도", example = "127.7777")
     private Double longitude;
