5d4975257b87008bedfd154608e046f9bf1f683e
Jang_Desktop <sdsd090811@gmail.com>
2023-09-17 11:50:51 +0900

    feat: 위치 관련 검증 로직 추가

--- src/main/java/com/map/gaja/client/presentation/ClientRequestValidator.java
+++ src/main/java/com/map/gaja/client/presentation/ClientRequestValidator.java
@@ -3,6 +3,7 @@ package com.map.gaja.client.presentation;
 import com.map.gaja.client.infrastructure.file.FileValidator;
 import com.map.gaja.client.infrastructure.file.exception.FileNotAllowedException;
 import com.map.gaja.client.presentation.dto.request.NewClientRequest;
+import com.map.gaja.client.presentation.dto.request.subdto.LocationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindException;
@@ -23,6 +24,7 @@ public class ClientRequestValidator {
             throw new BindException(bindingResult);
         }
 
+        validateLocationFields(clientRequest.getLocation(), bindingResult);
         validateBasicImage(clientRequest, bindingResult);
 
         // POST 요청시에는 기본 이미지가 아니라면 이미지가 필수로 있어야 한다.
@@ -42,6 +44,7 @@ public class ClientRequestValidator {
             throw new BindException(bindingResult);
         }
 
+        validateLocationFields(clientRequest.getLocation(), bindingResult);
         validateBasicImage(clientRequest, bindingResult);
         validateImageFile(clientRequest.getClientImage());
     }
@@ -64,6 +67,20 @@ public class ClientRequestValidator {
             bindingResult.addError(new ObjectError("newClientRequest", "사용자가 Basic Image를 사용 중이기 때문에 이미지 파일을 받을 수 없습니다."));
             throw new BindException(bindingResult);
         }
+    }
+
+    /**
+     * 위치 정보가 있다면 위도 경도가 둘 다 있어야 하고, 없다면 둘 다 없어야 한다.
+     */
+    private void validateLocationFields(LocationDto location, BindingResult bindingResult) throws BindException {
+        if (location == null
+                || (location.getLongitude() == null && location.getLatitude() == null)
+                || (location.getLongitude() != null && location.getLatitude() != null)) {
+            return;
+        }
+
+        bindingResult.addError(new ObjectError("newClientRequest", "위치 정보(위도, 경도)를 전부 입력하거나 입력하지 않아야 합니다."));
+        throw new BindException(bindingResult);
     }
 
     private boolean isNotEmptyFile(MultipartFile newImage) {
