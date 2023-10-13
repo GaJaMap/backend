ca680872c6b0b381de6bd9d6d62653675262c2ce
Jang_Desktop <sdsd090811@gmail.com>
2023-10-13 19:12:41 +0900

    test: ClientImageAuthAspect 테스트 코드 추가

---
+++ src/test/java/com/map/gaja/client/apllication/aop/ClientImageAuthAspectTest.java
@@ -0,0 +1,80 @@
+package com.map.gaja.client.apllication.aop;
+
+import com.map.gaja.client.presentation.dto.request.NewClientRequest;
+import com.map.gaja.global.authentication.CurrentSecurityUserGetter;
+import com.map.gaja.global.authentication.PrincipalDetails;
+import com.map.gaja.user.domain.model.Authority;
+import org.aspectj.lang.JoinPoint;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ClientImageAuthAspectTest {
+
+    @Mock
+    private CurrentSecurityUserGetter userGetter;
+
+    @InjectMocks
+    private ClientImageAuthAspect clientImageAuthAspect;
+
+    private PrincipalDetails mockFreeUser;
+    private PrincipalDetails mockVipUser;
+
+    @BeforeEach
+    void beforeEach() {
+        mockFreeUser = new PrincipalDetails(1L, "test@example.com", Authority.FREE.toString());
+        mockVipUser = new PrincipalDetails(1L, "test@example.com", Authority.VIP.toString());
+    }
+
+    @Test
+    @DisplayName("기본 이미지를 사용하는 Free User")
+    void testFreeUser() throws Exception {
+        when(userGetter.getCurrentUser()).thenReturn(mockFreeUser);
+        JoinPoint joinPoint = mock(JoinPoint.class);
+        NewClientRequest clientRequest = new NewClientRequest();
+        clientRequest.setIsBasicImage(true);
+        Object[] args = {clientRequest};
+        Mockito.when(joinPoint.getArgs()).thenReturn(args);
+
+        clientImageAuthAspect.checkAuthority(joinPoint);
+    }
+
+    @Test
+    @DisplayName("추가 이미지를 사용하는 VIP User")
+    void testVipUser() throws Exception {
+        when(userGetter.getCurrentUser()).thenReturn(mockVipUser);
+        JoinPoint joinPoint = mock(JoinPoint.class);
+        NewClientRequest clientRequest = new NewClientRequest();
+        clientRequest.setIsBasicImage(false);
+        Object[] args = {clientRequest};
+        Mockito.when(joinPoint.getArgs()).thenReturn(args);
+
+        clientImageAuthAspect.checkAuthority(joinPoint);
+    }
+
+    @Test
+    @DisplayName("추가 이미지를 사용하는 Free User")
+    void testFreeUserUsingImage() throws Exception {
+        when(userGetter.getCurrentUser()).thenReturn(mockFreeUser);
+        JoinPoint joinPoint = mock(JoinPoint.class);
+        NewClientRequest clientRequest = new NewClientRequest();
+        clientRequest.setIsBasicImage(false);
+        Object[] args = {clientRequest};
+        Mockito.when(joinPoint.getArgs()).thenReturn(args);
+
+        assertThrows(RuntimeException.class,
+                () -> clientImageAuthAspect.checkAuthority(joinPoint));
+    }
+
+}
