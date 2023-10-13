02e7d6fa0ce0a1a3fd3d7c82c97795b6ad4bfbe7
Jang_Desktop <sdsd090811@gmail.com>
2023-10-13 19:12:14 +0900

    fix: 기존 코드 수정 및 시큐리티 관련 컴포넌트 주입

--- src/main/java/com/map/gaja/client/apllication/aop/ClientImageAuthAspect.java
+++ src/main/java/com/map/gaja/client/apllication/aop/ClientImageAuthAspect.java
@@ -1,6 +1,7 @@
 package com.map.gaja.client.apllication.aop;
 
 import com.map.gaja.client.presentation.dto.request.NewClientRequest;
+import com.map.gaja.global.authentication.CurrentSecurityUserGetter;
 import com.map.gaja.global.authentication.PrincipalDetails;
 import com.map.gaja.user.domain.model.Authority;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,6 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Iterator;
@@ -20,10 +20,25 @@ import java.util.Iterator;
 @Component
 public class ClientImageAuthAspect {
 
-    @Before("( @within(com.map.gaja.client.apllication.aop.ClientImageAuth) " +
-            "|| @annotation(com.map.gaja.client.apllication.aop.ClientImageAuth) ) ")
+    private final CurrentSecurityUserGetter userGetter;
+
+    @Before("@within(com.map.gaja.client.apllication.aop.ClientImageAuth) " +
+            "|| @annotation(com.map.gaja.client.apllication.aop.ClientImageAuth)")
     public void checkAuthority(JoinPoint jp) throws Exception {
-        Object[] args = jp.getArgs();
+        NewClientRequest clientRequest = getClientRequestArgs(jp.getArgs());
+        if (clientRequest == null) {
+            return;
+        }
+
+        PrincipalDetails detail = userGetter.getCurrentUser();
+        System.out.println(isFreeAuth(detail));
+        System.out.println(isImageUpdateRequest(clientRequest));
+        if (isFreeAuth(detail) && isImageUpdateRequest(clientRequest)) {
+            throw new RuntimeException("커스텀 예외 필요");
+        }
+    }
+
+    private NewClientRequest getClientRequestArgs(Object[] args) {
         NewClientRequest clientRequest = null;
         for (Object arg : args) {
             if (arg instanceof NewClientRequest) {
@@ -31,22 +46,14 @@ public class ClientImageAuthAspect {
                 break;
             }
         }
-
-        if (clientRequest == null) {
-            return;
-        }
-
-        PrincipalDetails detail = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (isFreeAuth(detail) && isImageUpdateRequest(clientRequest)) {
-            throw new RuntimeException("커스텀 예외 필요");
-        }
+        return clientRequest;
     }
 
     private boolean isImageUpdateRequest(NewClientRequest clientRequest) {
         return clientRequest != null && !clientRequest.getIsBasicImage();
     }
 
-    private static boolean isFreeAuth(PrincipalDetails detail) {
+    private boolean isFreeAuth(PrincipalDetails detail) {
         Iterator<? extends GrantedAuthority> iterator = detail.getAuthorities().iterator();
         while (iterator.hasNext()) {
             if (iterator.next().getAuthority().equals(Authority.FREE.toString())) {
