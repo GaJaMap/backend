bf98d44d767c73dcee07de01e52d358ccaf0fc07
Jang_Desktop <sdsd090811@gmail.com>
2023-10-31 19:39:01 +0900

    fix: CurrentSecurityUserGetter에서 인증 안된 사용자는 null을 반환하도록 수정

--- src/main/java/com/map/gaja/global/authentication/CurrentSecurityUserGetter.java
+++ src/main/java/com/map/gaja/global/authentication/CurrentSecurityUserGetter.java
@@ -1,11 +1,27 @@
 package com.map.gaja.global.authentication;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+/**
+ * 현재 요청한 사용자의 세션 정보를 가져오는 컴포넌트.
+ *
+ * 무조건 인증된 사용자가 있을 때 사용할 것.
+ * 인증되지 않은 사용자가 있을 때는 NULL 반환으로 인한 NPE 위험이 있음.
+ */
 @Component
 public class CurrentSecurityUserGetter {
+
+    /**
+     * 현재 요청한 사용자의 세션 정보를 가져온다.
+     */
     public PrincipalDetails getCurrentUser() {
-        return (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated())
+            return null;
+
+        return (PrincipalDetails) authentication.getPrincipal();
     }
+
 }
