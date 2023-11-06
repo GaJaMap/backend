b8077c8a1171515a910db9f89c2137a90ab60e4b
Sangin <ksiisk99@gmail.com>
2023-11-06 15:31:04 +0900

    refactor: 중복 세션 제거 코드 리팩터링

--- src/main/java/com/map/gaja/global/authentication/SessionHandler.java
+++ src/main/java/com/map/gaja/global/authentication/SessionHandler.java
@@ -17,23 +17,16 @@ public class SessionHandler {
     private final FindByIndexNameSessionRepository findByIndexNameSessionRepository;
     private final SessionRepository sessionRepository;
 
+    /**
+     * 앱 or 웹 플랫폼에 중복된 세션이 있으면 제거
+     */
     @Transactional
     public void deduplicate(String email, String platformType) {
-        //해당 이메일에 속한 세션 모두 가져오기
-        Map<String, ? extends Session> sessions = findByIndexNameSessionRepository.findByPrincipalName(email);
+        Map<String, ? extends Session> sessions = getSessions(email); //해당 이메일에 속한 세션 모두 가져오기
 
-        if (sessions.isEmpty()) {
-            return;
-        }
-
-        for (Session session : sessions.values()) {
-            PrincipalDetails principalDetails = extractPrincipalDetails(session);
-
-            // 같은 플랫폼(웹 or 앱)에서 로그인을 한 적이 있다면 중복로그인
-            if (isDuplicate(platformType, principalDetails.getPlatformType())) {
-                sessionRepository.deleteById(session.getId()); //중복 로그인으로 이전 세션 제거
-            }
-        }
+        sessions.values().stream()
+                .filter(session -> isDuplicate(platformType, extractPrincipalDetails(session).getPlatformType())) // 같은 플랫폼(웹 or 앱)에서 로그인을 한 적이 있다면 중복로그인
+                .forEach(session -> sessionRepository.deleteById(session.getId())); //중복 로그인으로 이전 세션 제거
     }
 
     /**
