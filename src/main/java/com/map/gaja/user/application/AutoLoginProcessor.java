bae672949b20f3a81d0cab16f552f2cab2fa724f
Sangin <ksiisk99@gmail.com>
2023-12-31 00:00:34 +0900

    refactor: 자동로그인에서 최근 접속일과 최근 참조 그룹에 속한 클라이언트 조회 로직 분리

--- src/main/java/com/map/gaja/user/application/AutoLoginProcessor.java
+++ src/main/java/com/map/gaja/user/application/AutoLoginProcessor.java
@@ -10,13 +10,13 @@ import com.map.gaja.group.infrastructure.GroupRepository;
 import com.map.gaja.group.presentation.dto.response.GroupInfo;
 import com.map.gaja.user.domain.model.User;
 import com.map.gaja.user.infrastructure.UserRepository;
+import com.map.gaja.user.presentation.dto.ReferenceGroupId;
 import com.map.gaja.user.presentation.dto.response.AutoLoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 import static com.map.gaja.user.application.UserServiceHelper.findByEmailAndActive;
 import static com.map.gaja.user.constant.UserConstant.WHOLE_GROUP_INFO;
@@ -29,26 +29,25 @@ public class AutoLoginProcessor {
     private final UserRepository userRepository;
     private final S3UrlGenerator s3UrlGenerator;
 
-    /**
-     * 앱 실행 시 자동로그인에 대한 응답으로 사용자 최근 접속일도 처리.
-     *
-     * @param email 사용자 이메일
-     * @return 최근에 참조한 그룹정보와 그 그룹에 속한 클라이언트
-     */
     @Transactional
-    public AutoLoginResponse process(String email) {
+    public ReferenceGroupId login(String email) {
         User user = findByEmailAndActive(userRepository, email);
+
         user.updateLastLoginDate();
 
-        Long referenceGroupId = user.getReferenceGroupId();
-        GroupInfo referenceGroupInfo = getReferenceGroupInfo(referenceGroupId);
+        return new ReferenceGroupId(user.getId());
+    }
 
-        ClientListResponse recentGroupClients = getRecentGroupClients(email, referenceGroupId);
+    @Transactional(readOnly = true)
+    public AutoLoginResponse findReferenceGroupInClients(String email, ReferenceGroupId referenceGroupId) {
+        GroupInfo referenceGroupInfo = getReferenceGroupInfo(referenceGroupId.getId());
+
+        ClientListResponse recentGroupClients = getReferenceGroupClients(email, referenceGroupId.getId());
 
         return new AutoLoginResponse(recentGroupClients, s3UrlGenerator.getS3Url(), referenceGroupInfo);
     }
 
-    private ClientListResponse getRecentGroupClients(String email, Long referenceGroupId) {
+    private ClientListResponse getReferenceGroupClients(String email, Long referenceGroupId) {
         if (isWholeGroup(referenceGroupId)) {
             List<ClientOverviewResponse> clientList = clientQueryRepository.findActiveClientByEmail(email, null);
             return new ClientListResponse(clientList);
