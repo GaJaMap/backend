17f69870b4d7f19323efadcc073df169e5b39918
Sangin <ksiisk99@gmail.com>
2024-01-07 20:40:44 +0900

    fix: CircuitBreaker 실패율 계산하는 방식 Time-based에서 Count-based 기반으로 변경

--- src/main/java/com/map/gaja/global/config/CircuitConfig.java
+++ src/main/java/com/map/gaja/global/config/CircuitConfig.java
@@ -14,26 +14,24 @@ import java.time.Duration;
 @Configuration
 public class CircuitConfig {
     private final int FAILURE_RATE_THRESHOLD = 100;
-    private final int MINIMUM_NUMBER_OF_CALLS = 3;
     private final long WAIT_DURATION_IN_OPEN_STATE_MILLIS = 180000L;
     private final int PERMITTED_NUMBER_OF_CALLS_IN_HALF_OPEN_SATE = 1;
-    private final int SLIDING_WINDOW_SIZE = 5;
+    private final int SLIDING_WINDOW_SIZE = 4;
     private final int SLOW_CALL_RATE_THRESHOLD = 10;
     private final int SLOW_CALL_DURATION_THRESHOLD_SECONDS = 30;
 
     private CircuitBreakerConfig geoApiCircuitBreakerConfig() {
         return CircuitBreakerConfig.custom()
                 .failureRateThreshold(FAILURE_RATE_THRESHOLD)
-                .minimumNumberOfCalls(MINIMUM_NUMBER_OF_CALLS)
                 .slowCallRateThreshold(SLOW_CALL_RATE_THRESHOLD)
                 .slowCallDurationThreshold(Duration.ofSeconds(SLOW_CALL_DURATION_THRESHOLD_SECONDS))
                 .waitDurationInOpenState(Duration.ofMillis(WAIT_DURATION_IN_OPEN_STATE_MILLIS))
                 .permittedNumberOfCallsInHalfOpenState(PERMITTED_NUMBER_OF_CALLS_IN_HALF_OPEN_SATE)
-                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
                 .slidingWindowSize(SLIDING_WINDOW_SIZE)
                 .recordExceptions(WebClientException.class, IOException.class, TimeoutException.class)
                 .build();
     }
+
 
     private CircuitBreakerRegistry geoApiCircuitBreakerRegistry() {
         return CircuitBreakerRegistry.of(geoApiCircuitBreakerConfig());
