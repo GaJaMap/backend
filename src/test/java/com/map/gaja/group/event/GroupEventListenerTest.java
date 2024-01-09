195a1910e14d237321b81b17c806efdb8d4c6fcc
Sangin <ksiisk99@gmail.com>
2024-01-09 18:58:23 +0900

    test: 그룹 생성 도메인 이벤트 테스트

---
+++ src/test/java/com/map/gaja/group/event/GroupEventListenerTest.java
@@ -0,0 +1,36 @@
+package com.map.gaja.group.event;
+
+import com.map.gaja.global.event.Events;
+import com.map.gaja.user.domain.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@Transactional
+class GroupEventListenerTest {
+    @Autowired
+    ApplicationEventPublisher publisher;
+
+    @MockBean
+    GroupEventListener groupEventListener;
+
+    @Test
+    @DisplayName("그룹이 생성되면 이벤트 리스너가 실행된다.")
+    void groupCreateEvent() {
+        //given
+        GroupCreatedEvent groupCreatedEvent = new GroupCreatedEvent(new User("test"));
+
+        //when
+        Events.raise(groupCreatedEvent);
+
+        //then
+        verify(groupEventListener).create(groupCreatedEvent);
+    }
+}
