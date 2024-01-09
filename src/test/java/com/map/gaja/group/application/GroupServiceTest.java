d19fcb47a50fae75a0da3a38918af2df0e575fc5
Sangin <ksiisk99@gmail.com>
2024-01-10 01:07:32 +0900

    test: 도메인 이벤트 추가됨에 따라 GroupService 테스트 코드 변경

--- src/test/java/com/map/gaja/group/application/GroupServiceTest.java
+++ src/test/java/com/map/gaja/group/application/GroupServiceTest.java
@@ -6,7 +6,7 @@ import com.map.gaja.group.domain.service.GroupCommandService;
 import com.map.gaja.group.infrastructure.GroupRepository;
 import com.map.gaja.group.presentation.dto.request.GroupCreateRequest;
 import com.map.gaja.group.presentation.dto.request.GroupUpdateRequest;
-import com.map.gaja.user.domain.model.Authority;
+
 import com.map.gaja.user.domain.model.User;
 import com.map.gaja.user.infrastructure.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -16,13 +16,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,80 +41,59 @@ class GroupServiceTest {
     @Test
     @DisplayName("그룹 생성 성공")
     void createGroupSuccess() {
-        String email = "test@gmail.com";
+        // given
         GroupCreateRequest groupCreateRequest = new GroupCreateRequest("group");
-        User user = new User(email);
-
-        when(userRepository.findByEmailAndActiveForUpdate(user.getId())).thenReturn(Optional.of(user));
-        when(groupCommandService.create(any(), any())).thenReturn(new Group("group", user));
-
-        groupService.create(user.getId(), groupCreateRequest);
-
-        verify(groupRepository, times(1)).save(any());
-        assertEquals(1, user.getGroupCount());
-    }
-
-    @Test
-    @DisplayName("그룹 삭제 성공")
-    void deleteGroupSuccess() {
-        String email = "test@gmail.com";
-        User user = User.builder()
+        User user = new User("email");
+        Group group = Group.builder()
                 .id(1L)
-                .email(email)
-                .groupCount(1)
-                .active(true)
-                .authority(Authority.FREE)
-                .lastLoginDate(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
                 .build();
 
-        when(userRepository.findByEmailAndActiveForUpdate(user.getId())).thenReturn(Optional.of(user));
+        given(userRepository.findByEmailAndActiveForUpdate(anyLong()))
+                .willReturn(Optional.of(user));
+        given(groupCommandService.create(anyString(), any(User.class)))
+                .willReturn(group);
 
-        groupService.delete(user.getId(), 1L);
+        // when
+        Long groupId = groupService.create(1L, groupCreateRequest);
 
-        assertEquals(0, user.getGroupCount());
-
+        // then
+        verify(groupRepository).save(any(Group.class));
+        assertThat(groupId).isEqualTo(1L);
     }
 
     @Test
     @DisplayName("그룹명 변경 실패")
     void updateGroupNameFail() {
-        String email = "test@gmail.com";
-        User user = User.builder()
-                .id(1L)
-                .email(email)
-                .groupCount(0)
-                .active(true)
-                .authority(Authority.FREE)
-                .lastLoginDate(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
-                .build();
+        // given
+        User user = new User("email");
         GroupUpdateRequest request = new GroupUpdateRequest("test");
 
-        when(groupRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.empty());
+        given(groupRepository.findByIdAndUserId(anyLong(), anyLong()))
+                .willReturn(Optional.empty());
 
+        // when then
         assertThatThrownBy(() -> {
-            groupService.updateName(user.getId(), 1L, request);
-        })
-                .isInstanceOf(GroupNotFoundException.class);
+            groupService.updateName(1L, 1L, request);
+        }).isInstanceOf(GroupNotFoundException.class);
     }
 
     @Test
     @DisplayName("그룹명 변경 성공")
     void updateGroupNameSuccess() {
-        String email = "test@gmail.com";
-        User user = User.builder()
-                .id(1L)
-                .email(email)
-                .groupCount(0)
-                .active(true)
-                .authority(Authority.FREE)
-                .lastLoginDate(LocalDateTime.now(ZoneId.of("Asia/Seoul")))
+        // given
+        User user = new User("test");
+        GroupUpdateRequest request = new GroupUpdateRequest("update");
+        Group group = Group.builder()
+                .name("group")
                 .build();
-        GroupUpdateRequest request = new GroupUpdateRequest("test");
-        Group group = new Group("name", user);
 
-        when(groupRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of(group));
+        given(groupRepository.findByIdAndUserId(anyLong(), anyLong()))
+                .willReturn(Optional.of(group));
 
-        groupService.updateName(user.getId(), 1L, request);
-        assertEquals(request.getName(), group.getName());
+        // when
+        groupService.updateName(1L, 1L, request);
+
+        // then
+        assertThat(group.getName()).isEqualTo(request.getName());
     }
 }
