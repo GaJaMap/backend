2779394b94c82c1217ac8e0248c4e5f920cf6d55
Jang <sdsd090811@gmail.com>
2024-01-28 15:43:37 +0900

    feat: s3 저장 중 오류 발생시 세부 사항 로그 내용 추가

--- src/main/java/com/map/gaja/client/event/ClientImageListener.java
+++ src/main/java/com/map/gaja/client/event/ClientImageListener.java
@@ -21,13 +21,13 @@ public class ClientImageListener {
             phase = TransactionPhase.AFTER_COMMIT
     )
     public void storeImage(ClientImageCreationEvent event) {
+        ClientImage dbClientImage = event.getClientImage();
         try {
-            ClientImage dbClientImage = event.getClientImage();
             StoredFileDto fileDto = new StoredFileDto(dbClientImage.getSavedPath(), dbClientImage.getOriginalName());
             fileService.storeFile(fileDto, event.getImage());
         } catch (Exception e) {
             // TODO: 사용자에게 넘기거나 예외를 처리하거나 - 일단 무조건 사용자에게 책임 넘김
-            log.warn("이미지 관련 오류가 발생. clientImage에 저장된 임시 saved_path 필드 조치 필요");
+            log.warn("이미지 관련 오류가 발생. clientImage에 저장된 임시 saved_path 필드 조치 필요 : " + dbClientImage.getSavedPath(), e);
         }
     }
 }
