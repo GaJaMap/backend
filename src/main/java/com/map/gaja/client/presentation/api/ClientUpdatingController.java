34f78880afa25c5abe16d170a35199170941637f
Jang <sdsd090811@gmail.com>
2024-02-04 13:00:08 +0900

    refactor: Client컨트롤러 기능별 분리

--- src/main/java/com/map/gaja/client/presentation/api/ClientUpdatingController.java
+++ src/main/java/com/map/gaja/client/presentation/api/ClientUpdatingController.java
@@ -1,19 +1,19 @@
 package com.map.gaja.client.presentation.api;
 
-import com.map.gaja.client.application.*;
-import com.map.gaja.global.authentication.imageuploads.ImageAuthChecking;
+import com.map.gaja.client.application.ClientAccessVerifyService;
+import com.map.gaja.client.application.ClientBulkService;
+import com.map.gaja.client.application.ClientSavingService;
+import com.map.gaja.client.application.ClientUpdatingService;
 import com.map.gaja.client.application.validator.ClientRequestValidator;
-import com.map.gaja.client.presentation.api.specification.ClientCommandApiSpecification;
-import com.map.gaja.client.presentation.dto.access.ClientListAccessCheckDto;
-import com.map.gaja.client.presentation.dto.request.ClientIdsRequest;
-import com.map.gaja.client.presentation.dto.request.simple.SimpleClientBulkRequest;
-import com.map.gaja.client.presentation.dto.response.ClientOverviewResponse;
-import com.map.gaja.global.log.TimeCheckLog;
-import com.map.gaja.group.application.GroupAccessVerifyService;
 import com.map.gaja.client.infrastructure.s3.S3FileService;
+import com.map.gaja.client.presentation.api.specification.ClientUpdatingApiSpecification;
 import com.map.gaja.client.presentation.dto.access.ClientAccessCheckDto;
 import com.map.gaja.client.presentation.dto.request.NewClientRequest;
+import com.map.gaja.client.presentation.dto.response.ClientOverviewResponse;
 import com.map.gaja.client.presentation.dto.subdto.StoredFileDto;
+import com.map.gaja.global.authentication.imageuploads.ImageAuthChecking;
+import com.map.gaja.global.log.TimeCheckLog;
+import com.map.gaja.group.application.GroupAccessVerifyService;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Timed("client.modify")
 @Slf4j
@@ -34,47 +33,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-public class ClientController implements ClientCommandApiSpecification {
+public class ClientUpdatingController implements ClientUpdatingApiSpecification {
 
     private final ClientUpdatingService clientUpdatingService;
-    private final ClientBulkService clientBulkService;
     private final ClientAccessVerifyService clientAccessVerifyService;
     private final GroupAccessVerifyService groupAccessVerifyService;
     private final S3FileService fileService;
     private final ClientRequestValidator clientRequestValidator;
-    private final ClientSavingService clientSavingService;
-    private final ClientDeleteService clientDeleteService;
-
-
-    @DeleteMapping("/group/{groupId}/clients/{clientId}")
-    public ResponseEntity<Void> deleteClient(
-            @AuthenticationPrincipal(expression = "name") String loginEmail,
-            @PathVariable Long groupId,
-            @PathVariable Long clientId
-    ) {
-        // 특정 그룹 내에 거래처 삭제
-        ClientAccessCheckDto accessCheck = new ClientAccessCheckDto(loginEmail, groupId, clientId);
-        clientAccessVerifyService.verifyClientAccess(accessCheck);
-
-        clientDeleteService.deleteClient(clientId);
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @PostMapping("/group/{groupId}/clients/bulk-delete")
-    public ResponseEntity<Void> deleteBulkClient(
-            @AuthenticationPrincipal(expression = "name") String loginEmail,
-            @PathVariable Long groupId,
-            @Valid @RequestBody ClientIdsRequest clientIdsRequest
-    ) {
-        // 특정 그룹 내에 여러 거래처 삭제
-        ClientListAccessCheckDto accessCheck = new ClientListAccessCheckDto(loginEmail, groupId, clientIdsRequest.getClientIds());
-        clientAccessVerifyService.verifyClientListAccess(accessCheck);
-
-        clientBulkService.deleteBulkClient(groupId, clientIdsRequest.getClientIds());
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 
     @PutMapping("/group/{groupId}/clients/{clientId}")
     @ImageAuthChecking
@@ -128,47 +93,6 @@ public class ClientController implements ClientCommandApiSpecification {
         if (accessCheck.getGroupId() != clientRequest.getGroupId()) {
             groupAccessVerifyService.verifyGroupAccess(clientRequest.getGroupId(), accessCheck.getUserEmail());
         }
-    }
-
-    @PostMapping("/clients/bulk")
-    public ResponseEntity<List<Long>> addSimpleBulkClient(
-            @AuthenticationPrincipal(expression = "name") String loginEmail,
-            @Valid @RequestBody SimpleClientBulkRequest clientBulkRequest
-    ) {
-        // 단순한 Client 정보 등록
-        groupAccessVerifyService.verifyGroupAccess(clientBulkRequest.getGroupId(), loginEmail);
-        List<Long> response = clientBulkService.saveSimpleClientList(clientBulkRequest, loginEmail);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
-    }
-
-    /**
-     * ClientOverview를 반환하는 이유는
-     * 모바일에서 고객 등록 시에 바로 지도페이지로 넘어가기 때문에
-     * 반환받은 ClientOverview를 바로 리스트에 넣어서 사용해야 하기 때문이다.
-     * 저장된 프로필 이미지의 S3의 파일 경로와 생성된 고객 ID만 넘겨도 충분한가?
-     */
-    @PostMapping("/clients")
-    @ImageAuthChecking
-    public ResponseEntity<ClientOverviewResponse> addClient(
-            @AuthenticationPrincipal(expression = "name") String loginEmail,
-            @Valid @ModelAttribute NewClientRequest clientRequest,
-            BindingResult bindingResult
-    ) throws BindException {
-        // 거래처 등록 - 단건 등록
-        clientRequestValidator.validateNewClientRequestFields(clientRequest, bindingResult);
-        groupAccessVerifyService.verifyGroupAccess(clientRequest.getGroupId(), loginEmail);
-
-        if (isEmptyFile(clientRequest.getClientImage())) {
-            // 이미지를 등록하지 않음.
-            return new ResponseEntity<>(clientSavingService.saveClient(clientRequest, loginEmail), HttpStatus.CREATED);
-        }
-
-        ClientOverviewResponse response = clientSavingService.saveClientWithImage(clientRequest, loginEmail);
-        if (storeImage(clientRequest, response.getImage())) {
-            return new ResponseEntity<>(response, HttpStatus.CREATED);
-        }
-
-        return new ResponseEntity<>(response, HttpStatus.PARTIAL_CONTENT);
     }
 
     private boolean isEmptyFile(MultipartFile newImage) {
---
+++ src/main/java/com/map/gaja/client/presentation/api/specification/ClientUpdatingApiSpecification.java
@@ -0,0 +1,44 @@
+package com.map.gaja.client.presentation.api.specification;
+
+import com.map.gaja.client.presentation.dto.request.NewClientRequest;
+import com.map.gaja.client.presentation.dto.response.ClientOverviewResponse;
+import com.map.gaja.global.exception.ExceptionDto;
+import com.map.gaja.global.exception.ValidationErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.validation.Valid;
+
+public interface ClientUpdatingApiSpecification {
+    @Operation(summary = "고객 정보 변경", description = "form 형식으로 clientRequest 데이터를 전달해주세요.",
+            parameters = {
+                    @Parameter(name = "JSESSIONID", description = "세션 ID", in = ParameterIn.HEADER),
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ClientOverviewResponse.class))),
+                    @ApiResponse(responseCode = "206", description = "부분 성공 - 이미지 저장은 실패. DB에 저장 실패한 URL이 반환되기 때문에 사용자에게 변경을 권유해야함", content = @Content(schema = @Schema(implementation = ClientOverviewResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ValidationErrorResponse.class)))),
+                    @ApiResponse(responseCode = "403", description = "Free 등급 사용자 이미지 업로드 제한", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
+                    @ApiResponse(responseCode = "415", description = "서버에서 지원하지 않는 파일 형식", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
+                    @ApiResponse(responseCode = "422", description = "사용자에게 요청 그룹이 없거나, 그룹에 요청 고객이 없음 <br> 또는 그룹 내에 사용자 수보다 많은 사용자를 제거할 수 없음", content = @Content(schema = @Schema(implementation = ExceptionDto.class))),
+                    @ApiResponse(responseCode = "500", description = "서버 내부 오류로 이미지 저장 실패", content = @Content(schema = @Schema(implementation = ExceptionDto.class)))
+            })
+    ResponseEntity<ClientOverviewResponse> updateClient(
+            @Schema(hidden = true) @AuthenticationPrincipal String loginEmail,
+            @PathVariable Long groupId,
+            @PathVariable Long clientId,
+            @Valid @ModelAttribute NewClientRequest clientRequest,
+            @Schema(hidden = true) BindingResult bindingResult
+    ) throws BindException;
+}
